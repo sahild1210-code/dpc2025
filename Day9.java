dbcd3534980c6916ff82d85960b1ee56804133ff
sahild1210-code <sahild1210@gmail.com>
2025-08-23 22:44:43 +0530

    Create Day9.java
    Day9.challebge

---
+++ Day9.java
@@ -0,0 +1,21 @@
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0]; // take first string as initial prefix
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) { 
+                // reduce prefix until it matches
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println("Longest Common Prefix: " + longestCommonPrefix(strs));
+    }
+}
